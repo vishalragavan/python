20c2e3e4e384cf5f445dd03637c9108f40ece2d9
vishalragavan <74311574+vishalragavan@users.noreply.github.com>
2023-01-14 20:55:18 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,17 @@
+num = int(input())
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
